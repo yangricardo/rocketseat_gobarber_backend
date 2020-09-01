4d371f94fe920b54acec1f6f035b9b8b9d45d12a
Yang Ricardo Barcellos Miranda <yang_ricardo@hotmail.com>
2020-09-01 18:19:03 -0300

    gitignore

--- src/routes/index.ts
+++
@@ -1,9 +0,0 @@
-// src/routes/index.ts
-import { Router, request, response } from 'express';
-import appointments from './appointments';
-
-const routes = Router();
-
-routes.use('/appointments', appointments);
-
-export default routes;
