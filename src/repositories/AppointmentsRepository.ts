4ba36a5455b21d965c2aadd0a612d09a3a347db4
Yang Ricardo Barcellos Miranda <yang_ricardo@hotmail.com>
2020-09-01 19:23:56 -0300

    inicio da refatoração de repositorio

--- src/repositories/AppointmentsRepository.ts
+++ src/repositories/AppointmentsRepository.ts
@@ -7,27 +7,11 @@ interface CreateAppointmentDTO {
 }
 
 export default class AppointmentRepository {
-	private appointments: Appointment[];
-
-	constructor() {
-		this.appointments = [];
-	}
-
-	public create({ provider, date }: CreateAppointmentDTO): Appointment {
-		const appointment: Appointment = new Appointment({ provider, date });
-		this.appointments.push(appointment);
-		return appointment;
-	}
-
 	public findByDate(date: Date): Appointment | null {
 		const findAppointmentInSameDate = this.appointments.find(appointment =>
 			isEqual(date, appointment.date),
 		);
 
 		return findAppointmentInSameDate || null;
-	}
-
-	public all(): Appointment[] {
-		return this.appointments;
 	}
 }
