43ac9a6e4f287c9536e05335e7f1a35793312c2a
Yang Ricardo Barcellos Miranda <yang_ricardo@hotmail.com>
2020-09-02 20:16:07 -0300

    static files server

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,12 @@
-import express, { request, response } from 'express';
+import express from 'express';
 import './database';
 import routes from './routes';
+import uploadConfig from './config/upload';
 import 'reflect-metadata';
 
 const app = express();
 app.use(express.json());
+app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
 
 app.listen(3333, () => console.log('🐱‍🐉'));
