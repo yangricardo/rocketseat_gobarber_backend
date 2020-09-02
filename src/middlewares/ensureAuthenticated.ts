5f6813ea4cf4f6b895f7ffefedd2b2b98e8336a7
Yang Ricardo Barcellos Miranda <yang_ricardo@hotmail.com>
2020-09-01 22:04:05 -0300

    override de tipos

--- src/middlewares/ensureAuthenticated.ts
+++ src/middlewares/ensureAuthenticated.ts
@@ -1,6 +1,13 @@
 import { NextFunction, Request, Response } from 'express';
 import { verify } from 'jsonwebtoken';
 import authConfig from '../config/auth';
+
+interface TokenPayload {
+	iat: number;
+	exp: number;
+	sub: string;
+}
+
 export default function ensureAuthenticated(
 	request: Request,
 	response: Response,
@@ -13,7 +20,10 @@ export default function ensureAuthenticated(
 	const [, token] = authHeader.split(' ');
 	try {
 		const decoded = verify(token, authConfig.jwt.secret);
-		console.log(decoded);
+		const { sub } = decoded as TokenPayload;
+		request.user = {
+			id: sub,
+		};
 		return next();
 	} catch {
 		throw new Error(`Invalid JWT Token`);
